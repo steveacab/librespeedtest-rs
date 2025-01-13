a9548f1a7dbbf053e7a4551237530f3b23f09bcd
steveacab <tony_not@hotmail.it>
2025-01-13 14:20:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,13 +11,16 @@ set -e
 echo "Downloading and installing Librespeed-rs"
 wget -qO- https://github.com/librespeed/speedtest-rust/releases/download/v1.3.6/librespeed-rs-x86_64-unknown-linux-gnu.deb | dpkg -i -
 
-echo "Modifying index.html"
+echo "Downloading and replacing index.html"
 wget -qO /var/lib/librespeed-rs/assets/index.html https://raw.githubusercontent.com/steveacab/librespeedtest-rs/refs/heads/main/index.html
+
+echo "Opening configs.toml for editing. Please add your local IP address manually."
+echo "Press any key to continue when you're ready to edit..."
+read -n 1 -s
+
+nano /var/lib/librespeed-rs/configs.toml
 
 echo "Enabling and starting Librespeed-rs service"
 systemctl enable --now librespeed-rs.service
 
-echo "Add local IP address manually:  "
-nano /var/lib/librespeed-rs/configs.toml
-
-echo "Service commands:   start (sudo service librespeed-rs start),   stop (sudo service librespeed-rs stop),   status (sudo service librespeed-rs status)"
+echo "Installation and configuration complete! Service commands:  sudo service librespeed-rs start (start the serivce),  sudo service librespeed-rs stop (stop the service), sudo service librespeed-rs status (show the status of Librespeedtest-rs)"
